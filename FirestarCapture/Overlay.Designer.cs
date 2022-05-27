050ff071ae7923eb15a2911f8b75a422290b946f
F1restar4 <19477903+F1restar4@users.noreply.github.com>
2022-05-27 02:07:44 -0500

    initial commit

---
+++ FirestarCapture/Overlay.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace FirestarCapture
+{
+	partial class Overlay
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// Overlay
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.WindowText;
+			this.ClientSize = new System.Drawing.Size(1841, 891);
+			this.ControlBox = false;
+			this.MaximizeBox = false;
+			this.Name = "Overlay";
+			this.Opacity = 0.2D;
+			this.TopMost = true;
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# FirestarCapture
+A lightweight utility program for capturing defined portions of the screen as an image in your clipboard
+This only works on the primary monitor /shrug have fun sucks to be you I guess Micah
